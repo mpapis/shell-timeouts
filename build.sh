284ec92094de089b3d74cd70767eb8242efafc64
Michal Papis <mpapis@gmail.com>
2013-01-11 19:23:45 +0100

    make it work

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ travis_kill_children_processes()
     (( $# ))
   do
     travis_kill_children_processes $( travis_list_children $1 )
-    travis_safe_kill $1 -9
+    travis_safe_kill $1
     shift
   done
 }
@@ -38,15 +38,17 @@ detect_subshell_pid()
   _subshell_pid=$( ps -p $( ps -p $! -o ppid= ) -o ppid= )
 }
 
+trap "echo ''" TERM
 travis_timeout_function()
 (
-  echo "Timeout ($1 seconds) started for: $3"
-  trap "return 0" USR1
-  sleep $1
+  trap "return 0" TERM
   detect_subshell_pid
-  echo
-  echo "Timeout ($1 seconds) reached for: $3"
-  echo
+  typeset _counter
+  _counter=$1
+  while (( _counter-- > 0 ))
+  do    sleep 1s
+  done
+  printf "%b" "\nTimeout ($1 seconds) reached for: $3\n\n"
   travis_kill_children_processes $( travis_list_children $2 | grep -v "^[[:space:]]*${_subshell_pid}$" )
   travis_safe_kill $2
 )
@@ -60,9 +62,8 @@ travis_timeout()
   travis_timeout_function ${_my_timeout} $$ "$*" &
   _waiter_pid=$!
   echo "\$ $*"
-ps ajxf | awk $$'==$3{print}'
   "$@" || _return=$?
-  travis_safe_kill $_waiter_pid -USR1
+  travis_kill_children_processes $_waiter_pid
   return ${_return}
 }
 
@@ -72,4 +73,4 @@ _global_waiter_pid=$!
 travis_timeout 3 bundle install
 travis_timeout 3 bundle exec rake
 
-travis_safe_kill $_global_waiter_pid -USR1
+travis_kill_children_processes $_global_waiter_pid
